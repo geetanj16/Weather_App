cc8dea938f20e99f736fe5c08cf7749442332e4f
Geetanj <geetanj16sharma@gmail.com>
2023-07-01 17:31:36 +0530

    Added APi and query selector

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const apiKey = "7148702dd9f58cb8cda9b48610d1f834";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=bangalore";
 
-async function checjWeatehr(){
+async function checkWeather(){
     const response = await fetch(apiUrl + `&appid=${apiKey}`);
     var data = await response.json();
+    console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = data.main.temp;
+    document.querySelector(".humidity").innerHTML = data.main.humidity;
+    document.querySelector(".wind").innerHTML = data.wind.speed;
 }
+
+checkWeather();
