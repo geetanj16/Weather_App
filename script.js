720884f0402d3c0f6903e7590ee034cb9126e035
Geetanj <geetanj16sharma@gmail.com>
2023-07-04 13:19:18 +0530

    Search validation done

--- script.js
+++ script.js
@@ -10,31 +10,42 @@ document.addEventListener("DOMContentLoaded", () => {
 
     async function checkWeather(city) {
         const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-        var data = await response.json();
-        
-        document.querySelector(".city").innerHTML = data.name;
-        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
-        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-        document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
 
-        if(data.weather[0].main == "Clouds"){
-            weatherIcon.src = "img/clouds.png";
+        if (response.status == 404) {
+            document.querySelector(".error").style.display = "block";
+            document.querySelector(".weather").style.display = "none";
         }
 
-        else if(data.weather[0].main == "Clear"){
-            weatherIcon.src = "img/clear.png";
-        }
-        else if(data.weather[0].main == "Rain"){
-            weatherIcon.src = "img/rain.png";
-        }
-        else if(data.weather[0].main == "Drizzle"){
-            weatherIcon.src = "img/drizzle.png";
-        }
-        else if(data.weather[0].main == "Mist"){
-            weatherIcon.src = "img/mist.png";
+        else {
+            var data = await response.json();
+
+            document.querySelector(".city").innerHTML = data.name;
+            document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+            document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+            document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
+            if (data.weather[0].main == "Clouds") {
+                weatherIcon.src = "img/clouds.png";
+            }
+
+            else if (data.weather[0].main == "Clear") {
+                weatherIcon.src = "img/clear.png";
+            }
+            else if (data.weather[0].main == "Rain") {
+                weatherIcon.src = "img/rain.png";
+            }
+            else if (data.weather[0].main == "Drizzle") {
+                weatherIcon.src = "img/drizzle.png";
+            }
+            else if (data.weather[0].main == "Mist") {
+                weatherIcon.src = "img/mist.png";
+            }
+
+            document.querySelector(".weather").style.display = "block";
+            document.querySelector(".error").style.display = "none";
+
         }
 
-        document.querySelector(".weather").style.display = "block";
     }
 
     searchBtn.addEventListener("click", () => {
