38e457daad591d53cb97f37d49595f1916d9d7f5
Geetanj <geetanj16sharma@gmail.com>
2023-07-04 13:12:47 +0530

    Display none commit

--- script.js
+++ script.js
@@ -6,15 +6,35 @@ document.addEventListener("DOMContentLoaded", () => {
     const searchBox = document.querySelector(".search input");
     const searchBtn = document.querySelector(".search button");
 
+    const weatherIcon = document.querySelector(".weather-icon");
+
     async function checkWeather(city) {
         const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
         var data = await response.json();
-        console.log(data);
-
+        
         document.querySelector(".city").innerHTML = data.name;
         document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
         document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
         document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
+        if(data.weather[0].main == "Clouds"){
+            weatherIcon.src = "img/clouds.png";
+        }
+
+        else if(data.weather[0].main == "Clear"){
+            weatherIcon.src = "img/clear.png";
+        }
+        else if(data.weather[0].main == "Rain"){
+            weatherIcon.src = "img/rain.png";
+        }
+        else if(data.weather[0].main == "Drizzle"){
+            weatherIcon.src = "img/drizzle.png";
+        }
+        else if(data.weather[0].main == "Mist"){
+            weatherIcon.src = "img/mist.png";
+        }
+
+        document.querySelector(".weather").style.display = "block";
     }
 
     searchBtn.addEventListener("click", () => {
